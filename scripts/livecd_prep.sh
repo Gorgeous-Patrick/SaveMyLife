213ae27c552423392a36451e3656fe9dac9f4f57
Patrick Li <baichuanli@yahoo.com>
2023-03-07 22:29:05 -0500

    feat: initial scripts

---
+++ scripts/livecd_prep.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install vim
+
+sudo zpool import zroot -R /mnt -f
+sudo zfs mount zroot/ROOT/default
+sudo zfs mount -a
+sudo mount -o bind /dev /mnt/dev
+sudo mount -o bind /run /mnt/run
+sudo mount -o bind /sys /mnt/sys
+sudo mount -o bind /proc /mnt/proc
---
+++ scripts/livecd_umount.sh
@@ -0,0 +1,2 @@
+sudo umount -R /mnt
+sudo zpool export zroot
